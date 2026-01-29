4fb60de9178d6cdda7ee2f6f59073bd65c63b803
Pablo Calle <callejupa@gmail.com>
2026-01-29 15:52:38 -0500

    verify sudoku board in python

---
+++ problem-solving/13_verify_sudoku_board.py
@@ -0,0 +1,39 @@
+def verify_sudoku_board(board):
+    row_sets = [set() for _ in range(9)]
+    column_sets = [set() for _ in range(9)]
+    subgrid_sets = [[set() for _ in range(3)] for _ in range(3)]
+
+    for r in range(9):
+        for c in range(9):
+            num = board[r][c]
+            if num == 0:
+                continue
+
+            if num in row_sets[r]:
+                return False
+            if num in column_sets[c]:
+                return False
+            if num in subgrid_sets[r // 3][c // 3]:
+                return False
+            
+            row_sets[r].add(num)
+            column_sets[c].add(num)
+            subgrid_sets[r // 3][c // 3].add(num)
+    return True
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print(verify_sudoku_board(board))
+
