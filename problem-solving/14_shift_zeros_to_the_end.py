6f10e572ca7d0d7b6992f1fe7e4821c4bc691b5d
Pablo Calle <callejupa@gmail.com>
2026-01-30 17:20:33 -0500

    shift zeros to the end python kata

---
+++ problem-solving/14_shift_zeros_to_the_end.py
@@ -0,0 +1,22 @@
+# Given an array of integers, modify the array in place to move all zeros to the end while
+# maintaining the relative order of non-zero elements.
+# Example:
+# Input: nums = [0, 1, 0, 3, 2]
+# Output: [1, 3, 2, 0, 0]
+
+def shift_zeros_to_the_end(nums):
+    # The 'left' pointer is used to position non-zero elements.
+    left = 0
+    # Iterate through the array using a 'right' pointer to locate non-zero
+    # elements.
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            # Increment 'left' since it now points to a position already occupied
+            # by a non-zero element.
+            left += 1
+
+    return nums
+
+nums = [0, 1, 0, 3, 2]
+print(shift_zeros_to_the_end(nums))
