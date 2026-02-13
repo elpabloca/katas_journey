1b7e69cf7fb9b188462790547dc6ccf41d5134ce
Pablo Calle <callejupa@gmail.com>
2026-02-12 19:35:54 -0500

    casacding subsets in go

---
+++ problem-solving/01-cascading-subsets.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func eachConst(arr []int, sizeSubsetNumber int) [][]int {
+	var result [][]int
+
+	// calcular cuántos subarrays se pueden formar
+	for i := 0; i <= len(arr)-sizeSubsetNumber; i++ {
+		subset := arr[i : i+sizeSubsetNumber]
+		result = append(result, subset)
+		fmt.Println(result, "result array")
+	}
+
+	return result
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+
+	fmt.Println(eachConst(numbers, 2))
+	fmt.Println(eachConst(numbers, 3))
+}
---
+++ ydkjs/04-objects.js
@@ -0,0 +1,4 @@
+var strPrimitive = "I am a string";
+
+console.log(strPrimitive.length);   // 13
+console.log(strPrimitive.charAt(3)); // "m"
